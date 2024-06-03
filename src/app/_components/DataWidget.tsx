26e6139e3b721088d3bc0edd78cca16e6504ac4d
William <39624752+willmadd@users.noreply.github.com>
2024-06-03 20:41:56 +0100

    bits

---
+++ src/app/_components/DataWidget.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+
+type Props = {
+  action: (num: number) => Promise<string>;
+};
+
+const DataWidget = (props: Props) => {
+  const [message, setMessage] = React.useState<string | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    const data = await props.action(Math.random());
+    setLoading(false);
+    setMessage(data);
+    console.log(data);
+  };
+
+  return (
+    <div className="flex flex-col w-[300px]">
+      <button
+        className="w-full border border-solid border-black p-3 rounded-md bg-yellow-500 shadow-xl my-4"
+        onClick={handleClick}
+      >
+        Fetch Data
+      </button>
+      {loading ? (
+        <span>Loading...</span>
+      ) : message ? (
+        <div className="w-full border border-solid border-gray-500 bg-white p-3 rounded-md">
+          {message && <p>{message}</p>}
+        </div>
+      ) : (
+        "no number yet"
+      )}
+    </div>
+  );
+};
+
+export default DataWidget;
